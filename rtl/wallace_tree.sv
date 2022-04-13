3be04a35344e7407e4ba2f635ba8668dff960759
Qian Gu <guqian110@163.com>
2022-04-13 21:14:20 +0800

    fix(rtl): parameterize wallace_tree module

--- rtl/wallace_tree.sv
+++ rtl/wallace_tree.sv
@@ -10,8 +10,8 @@
 //
 //=============================================================================
 module wallace_tree #(
-  parameter DW = 16,
-  parameter PP = 5
+  parameter DW = 16,  // partial product data width
+  parameter PP = 5    // partial product number
 ) (
   input  logic                      pp_opt_i,
   input  logic [PP-1 : 0][DW-1 : 0] add_i,
@@ -27,37 +27,27 @@ module wallace_tree #(
   logic [LEVEL-1 : 0][DW-1 : 0] carry_shift;
 
   // wallace tree
-  // level0
-  for (genvar i = 0; i < DW; i++) begin : l_lvl0
-    assign {psum[0][i], carry[0][i]} = compressor32(add_i[0][i], add_i[1][i], add_i[2][i]);
-  end
-  // level1
-  for (genvar i = 0; i < DW; i++) begin : l_lvl1
-    assign {psum[1][i], carry[1][i]} = compressor32(psum[0][i], carry_shift[0][i], add_i[3][i]);
-  end
-  // level2
-  for (genvar i = 0; i < DW; i++) begin : l_lvl2
-    assign {psum[2][i], carry[2][i]} = compressor32(psum[1][i], carry_shift[1][i], add_i[4][i]);
+  for (genvar l = 0; l < LEVEL; l++) begin : l_compress
+    if (l == 0) begin : l_compress_1st
+      // compress
+      for (genvar i = 0; i < DW; i++) begin
+        assign {psum[l][i], carry[l][i]} = compressor32(add_i[0][i], add_i[1][i], add_i[2][i]);
+      end
+    end else begin : l_compress_others
+      // compress
+      for (genvar i = 0; i < DW; i++) begin
+        assign {psum[l][i], carry[l][i]} = compressor32(psum[l-1][i], carry_shift[l-1][i],
+                                                        add_i[l+2][i]);
+      end
+    end
+    // right shift carry 1bit for next level
+    assign carry_shift[l] = carry[l] << 1;
   end
 
-  assign carry_shift[0] = carry[0] << 1;
-  assign carry_shift[1] = carry[1] << 1;
-  assign carry_shift[2] = carry[2] << 1;
+  // output
+  assign sum_o = pp_opt_i ? psum[LEVEL-2] : psum[LEVEL-1];
+  assign carry_o = pp_opt_i ? carry_shift[LEVEL-2] : carry_shift[LEVEL-1];
 
-  assign sum_o = pp_opt_i ? psum[1] : psum[2];
-  assign carry_o = pp_opt_i ? carry_shift[1] : carry_shift[2];
-
-  // for (genvar l = 0; l < PP; l++) begin
-  //   if (l == 0) begin
-  //     assign psum[0] = pp[0];
-  //     assign psum[1] = pp[1];
-  //     assign psum[2] = pp[2];
-  //   end else begin
-  //     for (genvar i = 0; i < C_DW; i++) begin
-  //       assign {psum[l][i], carry[l][i]} = compressor32(psum[l][i], psum[l+1][i], psum[l+2][i]);
-  //     end
-  //   end
-  // end
 
   function automatic logic [1 : 0] compressor32(logic a, logic b, logic c);
 
